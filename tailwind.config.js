534e9543a5f22b236d7e34334a6d6daca68fd476
Igor Amantino <igor.amantinodev@hotmail.com>
2023-08-05 15:54:36 -0300

    feat/config-theme

--- tailwind.config.js
+++ tailwind.config.js
@@ -6,8 +6,25 @@ module.exports = {
     './src/app/**/*.{js,ts,jsx,tsx,mdx}',
   ],
   theme: {
-    extend: {
-      backgroundColor: '#FAFAFA',
+    colors: {
+      'yellow-dark': '#C47F17',
+      'yellow': '#DBAC2C',
+      'yellow-light': '#F1E9C9',
+      'purple-dark':'#4B2995',
+      'purple':'#8047F8',
+      'purple-light':'#EBE5F9',
+      'base-title':'#272221',
+      'base-subtitle':'#403937',
+      'base-text':'#574F4D',
+      'base-label':'#8D8686',
+      'base-hover': '#D7D5D5',
+      'base-button': '#E6E5e5',
+      'base-input': '#EDEDED',
+      'base-card': '#F3F2F2',
+
+
+
+
     },
   },
   plugins: [],
